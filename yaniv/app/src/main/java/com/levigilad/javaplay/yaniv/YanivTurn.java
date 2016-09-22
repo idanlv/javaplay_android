b54906568677b617661cbb3500c4aa969e24bf58
Maya <maiki89g@gmail.com>
2016-09-23 00:28:25 +0300

    Fixed compilation error Added the indexes of the cards for user retrieval to turn data

--- yaniv/app/src/main/java/com/levigilad/javaplay/yaniv/YanivTurn.java
+++ yaniv/app/src/main/java/com/levigilad/javaplay/yaniv/YanivTurn.java
@@ -1,18 +1,21 @@
 package com.levigilad.javaplay.yaniv;
 
-import com.levigilad.javaplay.infra.IJsonSerializable;
 import com.levigilad.javaplay.infra.Turn;
 import com.levigilad.javaplay.infra.entities.CardsDeck;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.LinkedList;
 
 /**
  * Created by User on 21/09/2016.
  */
 public class YanivTurn extends Turn {
     private CardsDeck _hiddenDeck = new CardsDeck();
-    private CardsDeck _exposedDeck = new CardsDeck();
+    private CardsDeck _discardedDeck = new CardsDeck();
+    private LinkedList<int> _availableDiscardedCards = new LinkedList<>();
     private boolean _initializeDone = false;
 
     /**
@@ -26,7 +29,15 @@ public class YanivTurn extends Turn {
 
         gameData.put("initializeDone", this._initializeDone);
         gameData.put("hiddenDeck", this._hiddenDeck.toJson());
-        gameData.put("exposedDeck", this._exposedDeck.toJson());
+        gameData.put("discardedDeck", this._discardedDeck.toJson());
+
+        JSONArray availableArray = new JSONArray();
+
+        for (int location : this._availableDiscardedCards) {
+            availableArray.put(availableArray);
+        }
+
+        gameData.put("available", availableArray);
 
         return gameData;
     }
@@ -38,11 +49,18 @@ public class YanivTurn extends Turn {
      */
     @Override
     public void fromJson(JSONObject object) throws JSONException {
-        this._initializeDone = data.getBoolean("initializeDone");
+        this._initializeDone = object.getBoolean("initializeDone");
         this._hiddenDeck = new CardsDeck();
-        this._hiddenDeck.fromJson(data.getJSONObject("hiddenDeck"));
-        this._exposedDeck = new CardsDeck();
-        this._exposedDeck.fromJson(data.getJSONObject("exposedDeck"));
+        this._hiddenDeck.fromJson(object.getJSONObject("hiddenDeck"));
+        this._discardedDeck = new CardsDeck();
+        this._discardedDeck.fromJson(object.getJSONObject("discardedDeck"));
+
+        this._availableDiscardedCards = new LinkedList<>();
+        JSONArray availableArray = object.getJSONArray("available");
+
+        for (int i = 0; i < availableArray.length(); i++) {
+            this._availableDiscardedCards.add(availableArray.getInt(i));
+        }
 
         super.fromJson(object);
     }
